21550f98cc538f189fae95d673d3c5e6a84ddeab
kumayoster <hhansen960@gmail.com>
2025-11-14 11:01:48 +0700

    tugas cerita

---
+++ pr_4/main5.py
@@ -0,0 +1,76 @@
+# nested = bersarang
+# dialam if / elif / else ada conditional
+
+
+# CERITA HORROR
+
+print("once upon at time at XII PPLG 1")
+print("Ada Gitar")
+print("1. Mainkan")
+print("2. Pulang dah capek")
+choice = int(input("= "))
+if choice == 1:
+  print("jreng jreng jreng jreng")
+  print("tiba tiba ada sosok hitam tinggi dari belakang")
+  print("1. Flying kick")
+  print("2. Run away")
+if choice == 2:
+  print("kamu pulang ke rumah dengan cara memanjat tembok di sekolah dan kamu tidak ketahuan oleh satpam")
+  print(" good ending 1")
+  exit()
+choice = int(input("= "))
+if choice == 1:
+  print("kamu tendang dan kepalanya jatuh ternyata itu manequin hitam yang di cat anak seni")
+  print("apa yang harus kamu lakukan next?")
+  print("1.Pulang")
+  print("2.tidur di kelas")
+if choice == 2:
+  print("kamu kabur dari kelas dan kamu ketahuan pak saptam gegara belom pulang sekolah ")
+  print("bad ending 1")
+  exit()
+choice = int(input("="))
+if choice == 1:
+  print("kamu pulang ke rumah dengan cara memanjat tembok di sekolah dan kamu tidak ketahuan oleh satpam")
+  print(" good ending 1")
+  exit()
+if choice == 2:
+  print("kamu tidur di kelas sampai pagi dan pagi paginya")
+  print("kamu kebangun jam 4 pagi gegara suara langkah kaki yang ada")
+  print("apa yang kamu lakukan")
+  print("1. lanjut tidur")
+  print("2. ambil hp")
+choice = int (input("="))
+if choice == 1:
+  print("kamu tertidur sampai besok pagi")
+  print("dan kamu sekolah kaya biasa")
+  print("sleep ending")
+  exit()
+if choice == 2:
+  print("kamu scroll hp")
+  print("dan menemukan artikel cara ngusir setan")
+  print("setelah itu kamu pede dan mencoba ngusir setan tapi pake apa")
+  print("1. bawang")
+  print("2. keyakinan")
+choice = int(input("="))
+if choice == 1:
+  print("kamu ke depan kelas dengan membawa bawang")
+  print("tapi kamu baru sadar di artikel ada 2 cara pake yang mana?")
+  print("1. bikin kalung dengan bawang")
+  print("2. lempat bawangnya ke setan")
+if choice == 2:
+  print("kamu pergi keluar dengan keyakinan kamu yang sangat bersarrrr")
+  print("kamu udah ketemu sosok hitam diruangan")
+  print("kamu langsung masuk ke ruangan")
+  print("ternyata itu teman kamu sedang numpang wifi sekolah buat download pubg")
+  exit()
+choice = int(input("="))
+if choice == 1:
+  print("kamu masuk ternyata itu teman kamu main ml dan kamu malu")
+  print("ending malu 1")
+if choice == 2:
+  print("kamu lempar bawangnya masuk")
+  print("tiba tiba ada suara gaduh")
+  print("kamu asuk keruangannya")
+  print("kamu ngeliat teman kamu ngibrit keluar dari sekolah")
+  print("ending teman laknat")
+  exit()
